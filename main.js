e20ce4aa9464a62b04102d54af87e2a785c65a73
choi2j <ritaliz1032@gmail.com>
2023-02-16 15:01:45 +0900

    final

---
+++ main.js
@@ -0,0 +1,31 @@
+document.body.addEventListener("mousemove", (evt) => {
+	const mouseX = evt.clientX;
+	const mouseY = evt.clientY;
+
+	gsap.set(".cursor", {
+		x: mouseX,
+		y: mouseY,
+	});
+
+	gsap.to(".shape", {
+		x: mouseX,
+		y: mouseY,
+		stagger: -0.1,
+	});
+});
+
+window.addEventListener("keydown", (text) => {
+	let t = text.key.toUpperCase();
+	let k = document.querySelector(`#${t}`);
+    if(k) {
+        k.classList.add("key-pressed");
+    }
+});
+
+window.addEventListener("keyup", (text) => {
+	let t = text.key.toUpperCase();
+	let k = document.querySelector(`#${t}`);
+    if(k) {
+        k.classList.remove("key-pressed");
+    }
+});
